879b6cde3a2289e497527f232b37bc7f48cd5216
bluemax <maxi@schremser.com>
2017-10-04 07:22:31 +0200

    implemented weight page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,42 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {WeightPage} from "../pages/weight/weight";
+import {GraphPage} from "../pages/graph/graph";
+import {StatsPage} from "../pages/stats/stats";
+import {WeightPageModule} from "../pages/weight/weight.module";
+import {GraphPageModule} from "../pages/graph/graph.module";
+import {StatsPageModule} from "../pages/stats/stats.module";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    //WeightPage,
+    //GraphPage,
+    //StatsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    WeightPageModule,
+    GraphPageModule,
+    StatsPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    WeightPage,
+    GraphPage,
+    StatsPage,
     TabsPage
   ],
   providers: [
@@ -37,4 +45,5 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import {WeightPage} from "../weight/weight";
+import {GraphPage} from "../graph/graph";
+import {StatsPage} from "../stats/stats";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = WeightPage;
+  tab2Root = GraphPage;
+  tab3Root = StatsPage;
 
   constructor() {
 
